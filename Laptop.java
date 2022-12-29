35aa329bc0da00800e4ae1cf4be0f97675cb7a3c
Konstantin <klik@mail.ru>
2022-12-29 20:16:09 +0300

    Final project

---
+++ Laptop.java
@@ -0,0 +1,39 @@
+public class Laptop {
+    private String name;
+    private String ram;
+    private String hardDisk;
+    private String operatingSystem;
+    private String colour;
+    
+    public Laptop(String name, String ram, String hardDisk, String operatingSystem, String colour) {
+        this.name = name;
+        this.ram = ram;
+        this.hardDisk = hardDisk;
+        this.operatingSystem = operatingSystem;
+        this.colour = colour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public String getHardDisk() {
+        return hardDisk;
+    }
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+    public String getColour() {
+        return colour;
+    }
+
+    // @Override
+    public String toString() {
+        return "(Ноутбук " + name + ", " + ram + "Gb, " + hardDisk + "Mb, "
+                + operatingSystem + ", " + colour + ")";
+    }
+}
